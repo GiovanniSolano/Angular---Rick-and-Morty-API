fb9ff1855988c3cb0e216fda147719db8c5577e3
Giovanni Solano <giovannisolano211@gmail.com>
2021-02-09 18:01:45 -0600

    Listado de personajes con paginación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonajesService } from './services/personajes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rickandmorty';
+
+
+  arrPersonajes: any[];
+  currentPage: number;
+  numPage: number;
+
+
+  constructor(private _personajesService: PersonajesService) {
+
+    this.currentPage = 1;
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+    this._personajesService.getAll().then(response => {
+      this.arrPersonajes = response['results'];
+      this.numPage = response['info']['pages'];
+    }).catch(err => console.log(err));
+
+  }
+
+  async changePage(pPage) {
+
+    this.currentPage += pPage;
+
+    (this.currentPage < 1) ? this.currentPage = 1 : this.currentPage;
+
+    const response = await this._personajesService.getAll(this.currentPage);
+    this.arrPersonajes = response['results'];
+    
+
+  }
+
 }
